f56e8e0dc6bb7d0b07978af5e47975634f1807cf
unknown <lethetai2605>
2021-03-23 11:27:28 +0700

    update

--- TKB/Controllers/HomeController.cs
+++ TKB/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TKB.Common;
 
 namespace TKB.Controllers
 {
@@ -10,6 +11,8 @@ namespace TKB.Controllers
     {
         public ActionResult Index()
         {
+            var session = (UserLogin)Session["USER_SESSION"];
+            ViewBag.UserName = session.UserName;
             return View();
         }
 
